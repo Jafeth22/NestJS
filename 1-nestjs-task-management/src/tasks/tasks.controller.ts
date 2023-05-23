b8b50a22441fd10ac38e617c915475313cec228c
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-05-22 21:18:22 -0600

    add functionality for task controller and services

--- 1-nestjs-task-management/src/tasks/tasks.controller.ts
+++ 1-nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  /**
+   * if we add private/public before the name, typeScript will take it
+   * as property/variable for this controller
+   */
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+}
