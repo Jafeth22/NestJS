095b95a28db4d4f2712763fc7b830e7f14d6245f
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-05-23 22:18:26 -0600

    update task method

--- 1-nestjs-task-management/src/tasks/tasks.controller.ts
+++ 1-nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { ITask } from './tasks.model';
+import { ITask, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -16,8 +24,30 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  /**
+   * When use the : it means that it will part of the path (it is a path param),
+   * so, it is a value that we can extract
+   */
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): ITask {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): ITask {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch(':id/status')
+  updateTask(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): ITask {
+    return this.tasksService.updateTask(id, status);
   }
 }
