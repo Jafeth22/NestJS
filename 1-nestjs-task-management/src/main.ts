48e4ec07a2f122fd8bb6e7c924ae235101254d60
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-05-24 20:56:45 -0600

    add validators (decorators) and pipes

--- 1-nestjs-task-management/src/main.ts
+++ 1-nestjs-task-management/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // AppModule = Route module of the app
   // await NestFactory.create() = Creates a new app
   const app = await NestFactory.create(AppModule);
+
+  // If NestJS encounters any the of valitations (decorators) that are in the app
+  // (this case in the DTO), it will know to execute validation, and it will save
+  // us from a lot of code creating valiations in the controller
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
