ab5e73b2f78fb5c5295ac09be1a61c48c9fcb279
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-05-30 21:10:21 -0600

    creating signin methods

--- 1-nestjs-task-management/src/auth/auth.service.ts
+++ 1-nestjs-task-management/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.reposity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,16 @@ export class AuthService {
 
   async singUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
+  }
+
+  async singIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRepository.findOne({ username });
+    const hashedPassword = await bcrypt.compare(password, user.password);
+
+    if (user && hashedPassword) {
+      return 'Success';
+    }
+    throw new UnauthorizedException('Please, check your login credentials');
   }
 }
