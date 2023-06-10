70ba8007995b2e519970a361a57b55586f31fb88
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-06-10 12:29:03 -0600

    update db conection with env variables

--- 1-nestjs-task-management/src/app.module.ts
+++ 1-nestjs-task-management/src/app.module.ts
@@ -2,26 +2,38 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    // Here we are initializing  the ConfigModule
     ConfigModule.forRoot({
       // Here we are using envFiles per environment, the order will import
       envFilePath: [`.env.stage.${process.env.STAGE}`],
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      // Nestjs will define entities, so it will translate tables and schemas
-      autoLoadEntities: true,
-      // It always keep the db schema sync with the help of TypeORM
-      synchronize: true,
+    // It uses Async because the values are not immediately when app starts, so,
+    // we need to wait for the config module to initialize
+    TypeOrmModule.forRootAsync({
+      // What modules do I depend?
+      imports: [ConfigModule],
+      // What I need to inject from those modules?
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        // it is call for nestJs when we want to initialize this module async
+        // This arrow func will return the config data
+        return {
+          type: 'postgres',
+          // Nestjs will define entities, so it will translate tables and schemas
+          autoLoadEntities: true,
+          // It always keep the db schema sync with the help of TypeORM
+          synchronize: true,
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
     }),
     AuthModule,
   ],
