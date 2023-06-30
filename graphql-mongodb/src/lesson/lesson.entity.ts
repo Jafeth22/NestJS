ecbbde2911bc374bd2a6a286634cec98cfaf1fa9
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-06-29 21:44:06 -0600

    add service and createLesson method

--- graphql-mongodb/src/lesson/lesson.entity.ts
+++ graphql-mongodb/src/lesson/lesson.entity.ts
@@ -3,6 +3,7 @@ import { Entity, Column, PrimaryColumn, ObjectIdColumn } from 'typeorm';
 @Entity()
 export class Lesson {
   // Internal object Id, so as not to expose the internal ID to the outside
+  // This is generated for mongodb
   @ObjectIdColumn()
   _id: string;
 
--- graphql-mongodb/src/lesson/lesson.resolver.ts
+++ graphql-mongodb/src/lesson/lesson.resolver.ts
@@ -1,13 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 /**
+ * ------------------------------------------------
+ * This is like a controller
+ * ------------------------------------------------
  * Here we define queries or mutations
  * queries = retrieves data
  * mutations = create new data or change existing
  */
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -16,5 +22,15 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    // this decorator retrieves individual arguments
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
