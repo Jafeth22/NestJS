cc714ef65b1f8e85624edf19e31700580c63d6de
Jafeth V Villarreal <33306997+Jafeth22@users.noreply.github.com>
2023-07-01 00:02:40 -0600

    refactor code

--- graphql-mongodb/src/student/student.resolver.ts
+++ graphql-mongodb/src/student/student.resolver.ts
@@ -3,21 +3,21 @@ import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
 import { StudentService } from './student.service';
 
-@Resolver((of) => StudentType)
+@Resolver(() => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
 
-  @Query((returns) => StudentType)
+  @Query(() => StudentType)
   async student(@Args('id') id: string) {
     return this.studentService.getStudent(id);
   }
 
-  @Query((returns) => [StudentType])
+  @Query(() => [StudentType])
   async students() {
     return this.studentService.getStudents();
   }
 
-  @Mutation((returns) => StudentType)
+  @Mutation(() => StudentType)
   async createStudent(
     @Args('createStudentInput') createStudentInput: CreateStudentInput,
   ) {
