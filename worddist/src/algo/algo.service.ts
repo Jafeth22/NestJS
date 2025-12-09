d22d66b7831ee437908239821ef4f9b0692e3cc9
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2025-12-08 21:25:36 -0600

    fix type errors

--- worddist/src/algo/algo.service.ts
+++ worddist/src/algo/algo.service.ts
@@ -1,18 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ALGO_MODULE_OPTIONS_TOKEN } from './algo.module-definition';
-import { AlgoModuleOptions } from './interfaces/algo.module-options';
-import { Algorithm } from "./interfaces/algorithm.interface";
+import type { AlgoModuleOptions } from './interfaces/algo.module-options';
+import { Algorithm } from './interfaces/algorithm.interface';
 import { contributors } from 'src/contributors/contributors';
 
 @Injectable()
 export class AlgoService {
-    private readonly algorithm: Algorithm;
+  private readonly algorithm: Algorithm;
 
-    constructor(@Inject(ALGO_MODULE_OPTIONS_TOKEN) private readonly options: AlgoModuleOptions) {
-        this.algorithm = contributors[options.contributor];
-    }
+  constructor(
+    @Inject(ALGO_MODULE_OPTIONS_TOKEN)
+    private readonly options: AlgoModuleOptions,
+  ) {
+    this.algorithm = contributors[options.contributor] as Algorithm;
+  }
 
-    getDistance(A: string, B: string): number {
-        return this.algorithm.getDistance(A, B);
-    }
+  getDistance(A: string, B: string): number {
+    return this.algorithm.getDistance(A, B);
+  }
 }
--- worddist/src/getdist/getdist.controller.ts
+++ worddist/src/getdist/getdist.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { Distance } from './interfaces/distance.interface';
+import type { Distance } from './interfaces/distance.interface';
 import { AlgoService } from 'src/algo/algo.service';
 
 @Controller('getdist')
 export class GetdistController {
-    constructor(private readonly algoService: AlgoService) {}
+  constructor(private readonly algoService: AlgoService) {}
 
-    @Get(":A/:B")
-    getDist(@Param("A") A: string, @Param("B") B: string): Distance {
-        return {
-            words: [A, B],
-            distance: this.algoService.getDistance(A, B)
-        };
-    }
+  @Get(':A/:B')
+  getDist(@Param('A') A: string, @Param('B') B: string): Distance {
+    return {
+      words: [A, B],
+      distance: this.algoService.getDistance(A, B),
+    };
+  }
 }
