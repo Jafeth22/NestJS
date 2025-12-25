8e855f52b7746e3058366b744988b962e4f7c46c
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2025-12-25 14:48:19 -0600

    middleware explanation, notes and example

--- middlewares/src/app.module.ts
+++ middlewares/src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+/**
+ * AppModule class that configures the LoggerMiddleware for all routes
+ * NestModule interface is implemented to gain access to the configure method
+ */
+export class AppModule implements NestModule {
+  // The configure method is used to apply middleware to routes
+  // MiddlewareConsumer automatically passes a helper class 'MiddlewareConsumer' to the configure method
+  configure(consumer: MiddlewareConsumer) {
+    // Here, LoggerMiddleware is applied to all routes using forRoutes('*')
+    consumer
+      .apply(LoggerMiddleware) // apply method is used to specify which middleware to use, it allows multiple middleware to be applied
+      .forRoutes('*'); // forRoutes method is used to specify the routes the middleware should apply to
+  }
+}
