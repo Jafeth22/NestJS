ff2ab2c96b312d5a51a8a889902112a2c15a5eb0
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2025-12-25 15:19:25 -0600

    using functional middleware

--- middlewares/src/app.module.ts
+++ middlewares/src/app.module.ts
@@ -1,7 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  /*MiddlewareConsumer,*/ Module /*, NestModule*/,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerMiddleware } from './logger/logger.middleware';
+// import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [],
@@ -12,16 +14,17 @@ import { LoggerMiddleware } from './logger/logger.middleware';
  * AppModule class that configures the LoggerMiddleware for all routes
  * NestModule interface is implemented to gain access to the configure method
  */
-export class AppModule implements NestModule {
-  // The configure method is used to apply middleware to routes
-  // MiddlewareConsumer automatically passes a helper class 'MiddlewareConsumer' to the configure method
-  configure(consumer: MiddlewareConsumer) {
-    // Here, LoggerMiddleware is applied to all routes using forRoutes('*')
-    consumer
-      .apply(LoggerMiddleware) // apply method is used to specify which middleware to use, it allows multiple middleware to be applied
-      // .exclude() // exclude method is used to specify routes that should be excluded from the middleware
-      .forRoutes('*'); // forRoutes method is used to specify the routes the middleware should apply to
-    // also, specific routes can be defined like { path: 'cats', method: RequestMethod.GET
-    // also, pass controllers instead of paths, or, mix both
-  }
-}
+// export class AppModule implements NestModule {
+//   // The configure method is used to apply middleware to routes
+//   // MiddlewareConsumer automatically passes a helper class 'MiddlewareConsumer' to the configure method
+//   configure(consumer: MiddlewareConsumer) {
+//     // Here, LoggerMiddleware is applied to all routes using forRoutes('*')
+//     consumer
+//       .apply(LoggerMiddleware) // apply method is used to specify which middleware to use, it allows multiple middleware to be applied
+//       // .exclude() // exclude method is used to specify routes that should be excluded from the middleware
+//       .forRoutes('*'); // forRoutes method is used to specify the routes the middleware should apply to
+//     // also, specific routes can be defined like { path: 'cats', method: RequestMethod.GET
+//     // also, pass controllers instead of paths, or, mix both
+//   }
+// }
+export class AppModule {}
--- middlewares/src/logger/logger.middleware.ts
+++ middlewares/src/logger/logger.middleware.ts
@@ -1,10 +1,25 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
-@Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(['this is the middleware', req.method, req.originalUrl]);
-    next();
-  }
+// It uses this attribute to define a class as a provider that can be injected as a dependency
+// When the middleware works for services, config, some logic or repositories, it should be a class-based middleware
+// @Injectable()
+// export class LoggerMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+//     console.log(['this is the middleware', req.method, req.originalUrl]);
+//     next();
+//   }
+// }
+
+/**
+ * Can be converted to functional because it has no dependencies
+ */
+export function logger(req: Request, res: Response, next: NextFunction) {
+  res.on('finish', () => {
+    console.log([
+      'This is the functional middleware',
+      `${req.method} - ${req.originalUrl} - ${res.statusCode} - ${res.statusMessage}`,
+    ]);
+  });
+  next();
 }
